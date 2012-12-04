bde848a19b62df66edb3327f62aac946e456af2b
Mustafa Shabib <mustafashabib@gmail.com>
2012-12-04 16:04:42 -0600

    deleted a ton of stuff. added new CCSprite+PerlinNoiseSprite folder with just the category files. Just drop that into your project - the PerlinNoiseDemo is a tiny app showing what it generates.

---
+++ CCSprite+PerlinNoiseSprite/CCSprite+PerlinNoiseSprite.h
@@ -0,0 +1,20 @@
+//
+//  PerlinNoiseSprite.h
+//  
+//
+//  Created by Mustafa Shabib on 4/5/12.
+//  Copyright 2012 . All rights reserved.
+//
+//  For now - generates a CCSprite using Perlin Noise
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CCSprite (PerlinNoiseSprite)
+{
+    
+}
+
+
++(CCSprite*) spriteWithPerlinNoise:(int)w:(int)h:(double)zoom:(double)p:(int)r:(int)g:(int)b:(int)r2:(int)g2:(int)b2:(int)octaves;
+@end
